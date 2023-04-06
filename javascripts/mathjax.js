9d170442d20c610591d7c72e6fbfb600de025bdf
Reza <mrezaalam@gmail.com>
2023-04-05 21:50:14 -0700

    Deployed cad1f54 with MkDocs version: 1.4.2

---
+++ javascripts/mathjax.js
@@ -0,0 +1,13 @@
+window.MathJax = {
+  tex: {
+    macros: {
+      T: "{\\bf R}",
+      w: "{\\mathscr w}",
+      P: "{\\mathscr P}",
+			ba: "\\begin{align}",
+			ea: "\\end{align}",
+			nn: "\\nonumber",
+    },
+  }
+};
+
